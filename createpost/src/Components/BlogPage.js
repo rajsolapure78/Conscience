4be61d5f41505236628636615d5d8f8dee7425de
rajsolapure78 <raj_solapure_78@yahoo.co.in>
2021-08-21 21:15:02 +0530

    CreatePost20Aug2021_1

---
+++ createpost/src/Components/BlogPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Components/Header'
+import Footer from './Components/Footer'
+
+export default function BlogPage() {
+    return (
+        <div>
+            <Header></Header>
+            This is Single Blog page.
+            <Footer></Footer>
+        </div>
+    )
+}
