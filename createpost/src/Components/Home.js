85c0a73c39c754a9f860ec5e9da58afd5a44eeb5
rajsolapure78 <raj_solapure_78@yahoo.co.in>
2021-08-25 12:17:44 +0530

    CreatePost25Aug2021_1

---
+++ createpost/src/Components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Hero from '../Components/Hero'
+import {Blogs} from '../Components/Blogs'
+export const Home = () => {
+    return (
+        <div>
+           <Hero/>
+           <Blogs/> 
+        </div>
+    )
+}
